c71f5f9892b26912b10a9aba83e10773631a46a0
gabriel henrique dos santos <gabrielHsantos2018@gmail.com>
2023-03-16 12:29:17 -0300

    placar inicial

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+valendo=1
+placart1=0
+placart2=0
 
   constructor() {}
+
+ mudarpeso(cont:number){
+this.valendo=cont;
+
+
+ }
+
+
+ somarplacart1(){
+this.placart1+=this.valendo
+
+ }
+
+ diminuir_placart1(){
+this.placart1-=this.valendo
+
+ }
+
+ somarplacart2(){
+  this.placart2+=this.valendo
+
+   }
+
+   diminuir_placart2(){
+  this.placart2-=this.valendo
+
+   }
+
 
 }
